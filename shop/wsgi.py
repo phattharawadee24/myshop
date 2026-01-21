7522a04877119f15e98dc51e8f08251590ebf499
phattharawadee24 <phattharawadeess283@gmail.com>
2026-01-21 10:43:49 +0700

    b

--- shop/wsgi.py
+++ shop/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.settings")
 
 app = get_wsgi_application()
