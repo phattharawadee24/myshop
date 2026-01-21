7f38292740de77fa49ed5d47fbd9aaae54846126
phattharawadee24 <phattharawadeess283@gmail.com>
2026-01-21 12:07:23 +0700

    pp3

--- shop/settings.py
+++ shop/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get(
     'SECRET_KEY', 
     'django-insecure-6x77jbj_2dh6azh=2j8nnkssuk=_0hd!gxpae&#5&+!y&u9=#v'
 )
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'shop.urls'
@@ -127,9 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # WhiteNoise Settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
