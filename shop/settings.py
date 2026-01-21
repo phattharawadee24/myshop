60c0d6540eaebb5dc22f47c137409509121b5d8c
phattharawadee24 <phattharawadeess283@gmail.com>
2026-01-21 10:39:18 +0700

    v

--- shop/settings.py
+++ shop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6x77jbj_2dh6azh=2j8nnkssuk=_0hd!gxpae&#5&+!y&u9=#v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", ".vercel.app"]
 
 
 # Application definition
--- shop/wsgi.py
+++ shop/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-application = get_wsgi_application()
+app = get_wsgi_application()
