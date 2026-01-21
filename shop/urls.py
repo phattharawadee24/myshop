71195824c59ba34a318f54038f9b065793cf883f
phattharawadee24 <phattharawadeess283@gmail.com>
2026-01-21 10:50:41 +0700

    p

--- shop/urls.py
+++ shop/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+    
+def home(request):
+    return HttpResponse("Hello Vercel")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('', include('inventory.urls')),
 ]
 
