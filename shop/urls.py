9a51ba29bcde47fd4794cd84103a92ada857cfa8
phattharawadee24 <phattharawadeess283@gmail.com>
2026-01-21 12:27:08 +0700

    Fix: Remove redirect loop from root URL

--- shop/urls.py
+++ shop/urls.py
@@ -18,16 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='login/', permanent=False)),
     path('', include('inventory.urls')),
 ]
 
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
