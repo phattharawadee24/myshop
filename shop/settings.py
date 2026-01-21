a3b5b38c79f139ddc1f8d45f662af5a6afda93f5
phattharawadee24 <phattharawadeess283@gmail.com>
2026-01-21 11:25:58 +0700

    Fix: Remove undefined imports in urls.py and simplify settings

--- shop/settings.py
+++ shop/settings.py
@@ -29,12 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [
-    '.vercel.app',
-    'localhost',
-    '127.0.0.1',
-    '*.vercel.app',
-]
+ALLOWED_HOSTS = ["*", ".vercel.app"]
 
 # Application definition
 
--- shop/urls.py
+++ shop/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
